53157938e77727d307326a7e4f07fa07bea74a01
Hideaki Sakai <hide@sakaiboz.com>
2023-07-11 13:54:48 +0900

    docs: ソースの頭のライセンス部分の修正

--- src/components/KomaSymbols.vue
+++ src/components/KomaSymbols.vue
@@ -3,18 +3,18 @@
     
     This file is part of Free-Shogi-Board-TS.
     
-    Free-Shogi-Board is free software: you can redistribute it and/or modify
+    Free-Shogi-Board-TS is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
     
-    Free-Shogi-Board is distributed in the hope that it will be useful,
+    Free-Shogi-Board-TS is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
     
     You should have received a copy of the GNU Affero General Public License
-    along with Free-Shogi-Board.  If not, see <https://www.gnu.org/licenses/>.
+    along with Free-Shogi-Board-TS.  If not, see <https://www.gnu.org/licenses/>.
 -->
 
 <template>
